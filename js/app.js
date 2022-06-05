1721b4c23f67f92a2fbb3c514fdaaf190f8024a4
Andiewahl <54854272+Andival-Sei@users.noreply.github.com>
2022-06-05 21:52:58 +0400

    123
    123

--- js/app.js
+++ js/app.js
@@ -493,6 +493,25 @@
         document.querySelector(".super-button2").classList.remove("active");
         document.querySelector(".close-menu").classList.remove("close-menu-active");
     }));
+    document.addEventListener("DOMContentLoaded", (() => {
+        const codeBlock = document.getElementById("code");
+        const copyButton = document.getElementById("copy-button");
+        const copySuccess = document.getElementById("copy-succes");
+        const copyTextHandler = () => {
+            const text = codeBlock.innerText;
+            var element = document.createElement("textarea");
+            document.body.appendChild(element);
+            element.value = text;
+            element.select();
+            document.execCommand("copy");
+            document.body.removeChild(element);
+            copySuccess.classList.add("show-message");
+            setTimeout((() => {
+                copySuccess.classList.remove("show-message");
+            }), 2500);
+        };
+        copyButton.addEventListener("click", copyTextHandler);
+    }));
     window["FLS"] = true;
     isWebp();
 })();
